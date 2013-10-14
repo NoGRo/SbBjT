47889d990b972c3b3c004b16d641a1721ae2e94d
NoGRo <dimnogro@gmail.com>
2013-10-14 19:18:10 -0300

    acomodando carpetas

--- SbBjT/Form1.Designer.cs
+++ SbBjT/Form1.Designer.cs
@@ -32,16 +32,14 @@
             SbBjT.Bussines.Duration duration1 = new SbBjT.Bussines.Duration();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.durSuck = new SbBjT.Controls.DurationSelect();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.camera1Combo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(425, 424);
+            this.button1.Location = new System.Drawing.Point(445, 470);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -51,23 +49,12 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(161, 342);
+            this.textBox1.Location = new System.Drawing.Point(371, 424);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(324, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::SbBjT.Properties.Resources.Empresario;
-            this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(53, 24);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(447, 312);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // durSuck
             // 
@@ -76,31 +63,41 @@
             duration1.Randomize = false;
             duration1.Value = 0;
             this.durSuck.Duration = duration1;
-            this.durSuck.Location = new System.Drawing.Point(127, 368);
+            this.durSuck.Location = new System.Drawing.Point(195, 470);
             this.durSuck.Metric = "Value:";
             this.durSuck.Name = "durSuck";
             this.durSuck.ShowMax = true;
             this.durSuck.ShowRandom = true;
-            this.durSuck.Size = new System.Drawing.Size(278, 50);
+            this.durSuck.Size = new System.Drawing.Size(207, 78);
             this.durSuck.TabIndex = 2;
             this.durSuck.Title = "Sucks";
             // 
-            // trackBar1
+            // videoSourcePlayer1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(340, 373);
-            this.trackBar1.Maximum = 255;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(244, 45);
-            this.trackBar1.TabIndex = 4;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.videoSourcePlayer1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.videoSourcePlayer1.ForeColor = System.Drawing.Color.White;
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(12, 39);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(459, 324);
+            this.videoSourcePlayer1.TabIndex = 5;
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // camera1Combo
+            // 
+            this.camera1Combo.FormattingEnabled = true;
+            this.camera1Combo.Location = new System.Drawing.Point(12, 12);
+            this.camera1Combo.Name = "camera1Combo";
+            this.camera1Combo.Size = new System.Drawing.Size(322, 21);
+            this.camera1Combo.TabIndex = 6;
+            this.camera1Combo.SelectedIndexChanged += new System.EventHandler(this.camera1Combo_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 452);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(835, 620);
+            this.Controls.Add(this.camera1Combo);
+            this.Controls.Add(this.videoSourcePlayer1);
             this.Controls.Add(this.durSuck);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -108,8 +105,6 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,8 +115,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private Controls.DurationSelect durSuck;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TrackBar trackBar1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox camera1Combo;
 
 
     }
--- SbBjT/Form1.cs
+++ SbBjT/Form1.cs
@@ -7,25 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using SbBjT.Bussines;
+using SbBjT.Bussines.ColorDetectorCore;
 using SbBjT.Bussines.BlowJobCore;
 using SbBjT.Bussines.VoiceCore;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace SbBjT
 {
     public partial class Form1 : Form
     {
         Master master = new Master();
-
+        FilterInfoCollection videoDevices;
         public Form1()
         {
             InitializeComponent();
+
+            try
+            {
+                // enumerate video devices
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                {
+                    throw new Exception();
+                }
+
+                for (int i = 1, n = videoDevices.Count; i <= n; i++)
+                {
+                    string cameraName = i + " : " + videoDevices[i - 1].Name;
+
+                    camera1Combo.Items.Add(cameraName);
+                }
+
+                camera1Combo.SelectedIndex = 0;
+            }
+            catch
+            {
+                camera1Combo.Items.Add("No cameras found");
+
+                camera1Combo.SelectedIndex = 0;
+
+                camera1Combo.Enabled = false;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             master.Voice = new Voice("Woman", "Woman");
             master.Dick = new Dick();
-            original = pictureBox1.Image;
+           
         }
 
 
@@ -37,7 +68,19 @@ namespace SbBjT
         }
 
         private void button1_Click_1(object sender, EventArgs e)
-        {            
+        {
+
+            VideoCaptureDevice videoSource1 = new VideoCaptureDevice(videoDevices[camera1Combo.SelectedIndex].MonikerString);
+            //videoSource1.DesiredFrameRate = 10;
+
+            videoSourcePlayer1.VideoSource = videoSource1;
+            //videoSourcePlayer1.Start();
+
+            Detector detector = new Detector();
+            detector.Video = videoSource1;
+            detector.Start();
+            
+            /*
             master.Personality =  new Personality();
             master.Personality.BlowJobType=  new BlowJobType();
             master.Personality.BlowJobType.Sucks = durSuck.Duration;
@@ -45,6 +88,7 @@ namespace SbBjT
             BlowJob blowJob = master.GetBlowJob();
             
             blowJob.Start();
+            */
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -54,34 +98,14 @@ namespace SbBjT
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            char key = (char)e.KeyChar;
-            switch (key.ToString())
-            {
-                case "1":
-                    master.Dick.FeelWhat(new DickPart(PartName.Out));
-                    break;
-                case "2":
-                    master.Dick.FeelWhat(new DickPart(PartName.Medium));
-                    break;
-                case "3":
-                    master.Dick.FeelWhat(new DickPart(PartName.Deep));
-                    break;
-            }
-
+     
         }
 
      
-
-
-        private void pictureBox1_Click(object sender, EventArgs e)
+        
+        private void camera1Combo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
-        }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            Tolerance = trackBar1.Value;
-            BuscarColor();
         }
 
     }
